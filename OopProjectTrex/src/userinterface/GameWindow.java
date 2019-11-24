8a8eb73f8d9111647b4bf76cb4a0e6b591688f56
DDolly <dd.delldolly@gmail.com>
2019-11-24 20:15:04 +0700

    add

--- OopProjectTrex/src/userinterface/GameWindow.java
+++ OopProjectTrex/src/userinterface/GameWindow.java
@@ -13,7 +13,7 @@ public class GameWindow extends JFrame {
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         
 	public GameWindow() {
-		super("Java T-Rex game");
+		super("Running Dino!");
 		setSize(SCREEN_WIDTH, 600);
 		setLocation(screenSize.width / 2 - SCREEN_WIDTH / 2, screenSize.height / 2 - 300);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
--- OopProjectTrex/src/userinterface/StartWindow.java
+++ OopProjectTrex/src/userinterface/StartWindow.java
@@ -7,6 +7,7 @@ package userinterface;
 
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -15,36 +16,39 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.*;
 
-/**
- *
- * @author delldolly
- */
+import OptionClass.Resource;
+import java.awt.LayoutManager;
+import java.awt.image.BufferedImage;
+
 public class StartWindow extends JFrame implements ActionListener{
     private String name;
     
     public static final int SCREEN_WIDTH = 1000;
     private GameWindow gameWindow;
     
+    private BufferedImage background;
+    private BufferedImage head1;
+    private BufferedImage head2;
+    
     private JPanel sp;
-    private JLabel lb;
     private JTextField n;
     private JButton start;
     Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
     
     public StartWindow() {
-        super("Dino Runner !!");
+        super("Running Dino!");
         setSize(SCREEN_WIDTH, 600);
         setLocation(screenSize.width / 2 - SCREEN_WIDTH / 2, screenSize.height / 2 - 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
         
+        background = Resource.getResouceImage("Game Element/bgStart.jpg");
+        head1 = Resource.getResouceImage("Game Element/running.png");
+        head2 = Resource.getResouceImage("Game Element/dino.png");
+        
         sp = new JPanel();
-        lb = new JLabel();
         n = new JTextField(20);
         start = new JButton("START");
-        
-        lb.setFont(new Font("Gurmukhi MN", Font.PLAIN, 40));
-        lb.setText("Please enter you name.");
         
         n.setFont(new Font("Gurmukhi MN", Font.PLAIN, 22));
         n.setHorizontalAlignment(JTextField.CENTER);
@@ -57,11 +61,10 @@ public class StartWindow extends JFrame implements ActionListener{
         getContentPane().setLayout(null);
         sp.setBounds(0, 0, 1000, 600);
         
-        lb.setBounds(285, 100, 700, 100);
-        n.setBounds(250, 230, 500, 70);
-        start.setBounds(400, 350, 200, 100);
+//        lb.setBounds(285, 100, 700, 100);
+        n.setBounds(250, 300, 500, 50);
+        start.setBounds(400, 400, 200, 100);
         
-        getContentPane().add(lb);
         getContentPane().add(n);
         getContentPane().add(start);
         
@@ -89,6 +92,12 @@ public class StartWindow extends JFrame implements ActionListener{
                 }
             }
         });
+    }
+    
+    public void paint(Graphics g) {
+        g.drawImage(background, 0, 0, null);
+        g.drawImage(head1, 240, 40, null);
+        g.drawImage(head2, 350, 170, null);
     }
     
     public void startPage() {
